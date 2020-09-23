f47a45aca285503a37c61cbbfadaaba27f3ae20a
andyPiersonCodes <71237914+andyPiersonCodes@users.noreply.github.com>
2020-09-22 21:45:40 -0400

    Added new operators, and functionality for them

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # web-calculator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Web based calculator. Created by Andy Pierson
+
+Change log:
+
+v.0.1 Can only do addition
+
+v.0.2 - added a drop down with operator selection
+        Can do *, %, +, - operations now.
+        What does ? do?.....
--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,45 @@
 
-function sum() {
-    var num1, num2, total;
+function calculate() {
+    var num1, num2, total, operatorDropDown, operatorType, submitted;
     num1 = parseInt(document.getElementById('number1').value);
     num2 = parseInt(document.getElementById('number2').value);
-    
-    total = num1 + num2;
+    operatorDropDown = document.getElementById('operator')
+    operatorType = operatorDropDown.options[operatorDropDown.selectedIndex].value
 
-    document.getElementById("answer").innerHTML = "The total is " + total;
+    switch (operatorType){
+        case 'plus':
+            total = num1 + num2;
+            document.getElementById("answer").innerHTML = "The total is " + total;
+            break
+            
+        case 'minus':
+            total = num1 - num2;
+            document.getElementById("answer").innerHTML = "The total is " + total;
+            break
+
+        case 'times':
+            total = num1 * num2;
+            document.getElementById("answer").innerHTML = "The total is " + total;
+            break
+
+        case 'divide':
+            total = num1 / num2;
+            document.getElementById("answer").innerHTML = "The total is " + total;
+            break
+
+        case 'whatsThis':
+            submitted = document.createElement("IMG");
+            submitted.setAttribute("src", "submitButton.png");
+            submitted.setAttribute("width", "304");
+            submitted.setAttribute("height", "228");
+            document.body.appendChild(submitted);
+            break
+
+        default:
+            document.getElementById("answer").innerHTML ="Please choose an operator";
+            break
+
+    }
+
+
 }
